89f1bfb35bdd9d8326a7576f6293be73494bb470
Danil_Titarenko <danil.titarenko@gmail.com>
2023-11-02 14:28:31 +0500

    task 2

--- Game/Domain/MongoUserRepositoty.cs
+++ Game/Domain/MongoUserRepositoty.cs
@@ -9,9 +9,12 @@ namespace Game.Domain
         private readonly IMongoCollection<UserEntity> userCollection;
         public const string CollectionName = "users";
 
+        [Obsolete("Obsolete")]
         public MongoUserRepository(IMongoDatabase database)
         {
             userCollection = database.GetCollection<UserEntity>(CollectionName);
+            var options = new CreateIndexOptions {Unique = true};
+            userCollection.Indexes.CreateOne("{Login: 1}", options);
         }
 
         public UserEntity Insert(UserEntity user)
