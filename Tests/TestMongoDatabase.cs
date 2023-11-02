6ff65daac510dc7b2f77e9cecde03f5f43822c89
3a4eM-MHE-HuK <gavrilov.nikita@urfu.me>
2023-11-02 14:30:24 +0500

    done all

--- Tests/TestMongoDatabase.cs
+++ Tests/TestMongoDatabase.cs
@@ -7,8 +7,7 @@ namespace Tests
     {
         public static IMongoDatabase Create()
         {
-            var mongoConnectionString = Environment.GetEnvironmentVariable("PROJECT5100_MONGO_CONNECTION_STRING")
-                                        ?? "mongodb://localhost:27017";
+            var mongoConnectionString = "mongodb+srv://cmbbigis:8UnHIHU9gzDY5Byz@kontur-backend-course.ijouauu.mongodb.net/";
             var mongoClient = new MongoClient(mongoConnectionString);
             return mongoClient.GetDatabase("game-tests");
         }
