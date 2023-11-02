94102c39c2c71b1a7cb08b978a1cbd72909e2c28
SBB <me.mashkin7@gmail.com>
2023-11-02 18:45:57 +0500

    1-3 tasks

--- Tests/TestMongoDatabase.cs
+++ Tests/TestMongoDatabase.cs
@@ -8,7 +8,7 @@ namespace Tests
         public static IMongoDatabase Create()
         {
             var mongoConnectionString = Environment.GetEnvironmentVariable("PROJECT5100_MONGO_CONNECTION_STRING")
-                                        ?? "mongodb://localhost:27017";
+                                        ?? "mongodb+srv://loginMongo:Q8I4k4zhefpOEq5M@cluster0.jwlsdu6.mongodb.net/";
             var mongoClient = new MongoClient(mongoConnectionString);
             return mongoClient.GetDatabase("game-tests");
         }
