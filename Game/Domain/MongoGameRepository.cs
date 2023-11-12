17f1839185a60987473db0ed3add4edbf7d08713
nikunis <nik.unisikhin.05@gmail.com>
2023-11-12 14:13:42 +0500

    done

--- Game/Domain/MongoGameRepository.cs
+++ Game/Domain/MongoGameRepository.cs
@@ -7,39 +7,51 @@ namespace Game.Domain
     // TODO Сделать по аналогии с MongoUserRepository
     public class MongoGameRepository : IGameRepository
     {
+        private readonly IMongoCollection<GameEntity> gameCollection;
         public const string CollectionName = "games";
 
-        public MongoGameRepository(IMongoDatabase db)
+        public MongoGameRepository(IMongoDatabase db, bool dropCollection = true)
         {
+            if (dropCollection)
+                db.DropCollection(CollectionName);
+
+            gameCollection = db.GetCollection<GameEntity>(CollectionName);
         }
 
         public GameEntity Insert(GameEntity game)
         {
-            throw new NotImplementedException();
+            gameCollection.InsertOne(game);
+            return game;
         }
 
         public GameEntity FindById(Guid gameId)
         {
-            throw new NotImplementedException();
+            var filter = Builders<GameEntity>.Filter.Eq(g => g.Id, gameId);
+            return gameCollection.Find(filter).FirstOrDefault();
         }
 
         public void Update(GameEntity game)
         {
-            throw new NotImplementedException();
+            var filter = Builders<GameEntity>.Filter.Eq(g => g.Id, game.Id);
+            gameCollection.ReplaceOne(filter, game);
         }
 
         // Возвращает не более чем limit игр со статусом GameStatus.WaitingToStart
         public IList<GameEntity> FindWaitingToStart(int limit)
         {
-            //TODO: Используй Find и Limit
-            throw new NotImplementedException();
+            var filter = Builders<GameEntity>.Filter.Eq(g => g.Status, GameStatus.WaitingToStart);
+            return gameCollection.Find(filter)
+                .Limit(limit)
+                .ToList();
         }
 
         // Обновляет игру, если она находится в статусе GameStatus.WaitingToStart
         public bool TryUpdateWaitingToStart(GameEntity game)
         {
-            //TODO: Для проверки успешности используй IsAcknowledged и ModifiedCount из результата
-            throw new NotImplementedException();
+            var filterBuilder = Builders<GameEntity>.Filter;
+            var filter = filterBuilder.Eq(g => g.Status, GameStatus.WaitingToStart) & filterBuilder.Eq(g => g.Id, game.Id);
+            var result = gameCollection.ReplaceOne(filter, game);
+            return result.IsAcknowledged && result.ModifiedCount == 1;
         }
     }
 }
--- Game/Domain/MongoGameTurnRepository.cs
+++ Game/Domain/MongoGameTurnRepository.cs
@@ -1,6 +1,43 @@
+using System;
+using System.Collections.Generic;
+using MongoDB.Driver;
+
 namespace Game.Domain
 {
     public class MongoGameTurnRepository : IGameTurnRepository
     {
+        private IMongoCollection<GameTurnEntity> turnsCollection;
+        public const string CollectionName = "turns";
+
+        public MongoGameTurnRepository(IMongoDatabase db, bool dropCollection = true)
+        {
+            if (dropCollection)
+                db.DropCollection(CollectionName);
+
+            turnsCollection = db.GetCollection<GameTurnEntity>(CollectionName);
+
+            var indexBuilder = Builders<GameTurnEntity>.IndexKeys.Ascending(entity => entity.GameId);
+            var createIndexModel = new CreateIndexModel<GameTurnEntity>(indexBuilder);
+            turnsCollection.Indexes.CreateOne(createIndexModel);
+            indexBuilder = Builders<GameTurnEntity>.IndexKeys.Descending(entity => entity.TurnIndex);
+            createIndexModel = new CreateIndexModel<GameTurnEntity>(indexBuilder);
+            turnsCollection.Indexes.CreateOne(createIndexModel);
+        }
+
+
+        public GameTurnEntity Insert(GameTurnEntity turn)
+        {
+            turnsCollection.InsertOne(turn);
+            return turn;
+        }
+
+        public IList<GameTurnEntity> FindLastTurns(Guid gameId, int turnsCount = 5)
+        {
+            var filterBuilder = Builders<GameTurnEntity>.Filter;
+            var filter = filterBuilder.Eq(entity => entity.GameId, gameId);
+            var lastTurns = turnsCollection.Find(filter).SortByDescending(t => t.TurnIndex).Limit(turnsCount).ToList();
+            lastTurns.Reverse();
+            return lastTurns;
+        }
     }
 }
