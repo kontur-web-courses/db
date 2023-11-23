16fe831242bd93a3c37c0d1e7d3dd7b6e6c4cb85
Akergez <ragusseven@gmail.com>
2023-11-23 19:26:52 +0500

    add user repository

--- Game/Domain/MongoUserRepositoty.cs
+++ Game/Domain/MongoUserRepositoty.cs
@@ -15,39 +15,62 @@ namespace Game.Domain
 
         public UserEntity Insert(UserEntity user)
         {
-            //TODO: Ищи в документации InsertXXX.
-            throw new NotImplementedException();
+            userCollection.InsertOne(user);
+            return user;
         }
 
         public UserEntity FindById(Guid id)
         {
-            //TODO: Ищи в документации FindXXX
-            throw new NotImplementedException();
+            return userCollection.Find(x => x.Id == id).FirstOrDefault();
         }
 
         public UserEntity GetOrCreateByLogin(string login)
         {
-            //TODO: Это Find или Insert
-            throw new NotImplementedException();
+            var user = userCollection.Find(x => x.Login == login).FirstOrDefault();
+            if (user != null) return user;
+            user = new UserEntity
+            {
+                Login = login
+            };
+            userCollection.InsertOne(user);
+
+            return user;
         }
 
         public void Update(UserEntity user)
         {
-            //TODO: Ищи в документации ReplaceXXX
-            throw new NotImplementedException();
+            userCollection
+                .ReplaceOne(x => x.Id == user.Id, user);
         }
 
         public void Delete(Guid id)
         {
-            throw new NotImplementedException();
+            userCollection.DeleteOne(x => x.Id == id);
         }
 
-        // Для вывода списка всех пользователей (упорядоченных по логину)
-        // страницы нумеруются с единицы
         public PageList<UserEntity> GetPage(int pageNumber, int pageSize)
         {
-            //TODO: Тебе понадобятся SortBy, Skip и Limit
-            throw new NotImplementedException();
+            var totalUsers = userCollection.CountDocuments(x => true);
+            var totalPages = totalUsers % pageSize == 0
+                ? totalUsers / pageSize
+                : totalUsers / pageSize + 1;
+
+            if (pageNumber < 1)
+            {
+                pageNumber = 1;
+            }
+            else if (pageNumber > totalPages)
+            {
+                pageNumber = (int)totalPages;
+            }
+
+            var users = userCollection.Find(x => true)
+                                      .SortBy(x => x.Login)
+                                      .Skip((pageNumber - 1) * pageSize)
+                                      .Limit(pageSize)
+                                      .ToList();
+
+            return new PageList<UserEntity>(users, totalUsers, pageNumber, pageSize);
         }
 
         // Не нужно реализовывать этот метод
