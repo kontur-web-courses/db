d0c3f3955cb2216157fc74cb7e6b1c1c55c487cc
Евгений <11@11.11>
2023-11-23 19:38:42 +0500

    включил использование репозиториев

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Linq;
 using Game.Domain;
-using MongoDB.Driver;
+using Tests;
 
 namespace ConsoleApp
 {
@@ -13,10 +13,10 @@ namespace ConsoleApp
 
         private Program(string[] args)
         {
-            var db = Create();
+            var db = TestMongoDatabase.Create();
             userRepo = new MongoUserRepository(db);
             gameRepo = new MongoGameRepository(db);
-        }
+      }
 
         public static void Main(string[] args)
         {
@@ -40,13 +40,6 @@ namespace ConsoleApp
 
             Console.WriteLine("Game is finished");
             Console.ReadLine();
-        }
-        public static IMongoDatabase Create()
-        {
-            var mongoConnectionString = Environment.GetEnvironmentVariable("PROJECT5100_MONGO_CONNECTION_STRING")
-                                        ?? "mongodb://myuser:mypassword@localhost:27017/?authSource=db";
-            var mongoClient = new MongoClient(mongoConnectionString);
-            return mongoClient.GetDatabase("db");
         }
 
         private GameEntity StartNewGame(UserEntity user)
